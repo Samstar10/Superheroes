c511b59d97a012e287af05cfe561b97d1caa14fb
Samuel Muli <samuelmuli95@gmail.com>
2024-01-14 18:26:03 +0300

    Create Hero model

--- app/models.py
+++ app/models.py
@@ -1,10 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Hero(db.Model):
-    __tablename__ = 'hero'
+class Hero(db.Model, SerializerMixin):
+    __tablename__ = 'heroes'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    super_name = db.Column(db.String(80), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
 
-# add any models you may need. 
+    def __repr__(self):
+        return f'<Hero {self.name}, {self.super_name}>'
+    
+
+ 
