6df52be97b92c3e8bb4544f1bb547451fb2e2893
Samuel Muli <samuelmuli95@gmail.com>
2024-01-14 23:14:05 +0300

    Update PATCH method for Powers By Id

--- app/app.py
+++ app/app.py
@@ -4,7 +4,6 @@ from flask import Flask, make_response, request, jsonify
 from flask_migrate import Migrate
 import os
 from flask_restful import Api, Resource
-from sqlalchemy.orm import joinedload
 
 from models import db, Hero, Power, HeroPower
 
@@ -37,14 +36,65 @@ class Heroes(Resource):
 
 class HeroesById(Resource):
     def get(self, id):
-        hero = Hero.query.filter_by(id=id).first().to_dict()
+        hero = Hero.query.filter_by(id=id).first()
 
-        return make_response(jsonify(hero), 200)
+        if hero:
+            hero_dict = hero.to_dict()
 
+            return make_response(jsonify(hero_dict), 200)
+        
+        else:
+            return make_response(jsonify({"error": "Hero not found"}), 404)
 
+class Powers(Resource):
+    def get(self):
+        powers = Power.query.all()
+        powers_dict = [
+            {
+                "id": power.id,
+                "name": power.name,
+                "description": power.description
+            } for power in powers
+        ]
+        
+        return make_response(jsonify(powers_dict), 200)
+    
+class PowersById(Resource):
+    def get(self, id):
+        power = Power.query.filter_by(id=id).first()
+
+        if power:
+            power_dict = power.to_dict()
+
+            return make_response(jsonify(power_dict), 200)
+        
+        else:
+            return make_response(jsonify({"error": "Power not found"}), 404)
+        
+    def patch(self, id):
+        power = Power.query.filter_by(id=id).first()
+
+        if power:
+            for attr in request.form:
+                setattr(power, attr, request.form[attr])
+
+            db.session.commit(power)
+
+            response_dict = {
+                "id": power.id,
+                "name": power.name,
+                "description": power.description
+            }
+
+            return make_response(jsonify(response_dict), 200)
+        
+        else:
+            return make_response(jsonify({"error": "Power not found"}), 404)
 
 api.add_resource(Heroes, '/heroes')
 api.add_resource(HeroesById, '/heroes/<int:id>')
+api.add_resource(Powers, '/powers')
+api.add_resource(PowersById, '/powers/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5556)
