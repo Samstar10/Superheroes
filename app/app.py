f712c97e306f059ff602516455d0f146e4169b9c
Samuel Muli <samuelmuli95@gmail.com>
2024-01-14 21:58:24 +0300

    Create GET /heroes

--- app/app.py
+++ app/app.py
@@ -1,25 +1,41 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, request, jsonify
 from flask_migrate import Migrate
 import os
+from flask_restful import Api, Resource
+from sqlalchemy.orm import joinedload
 
-from models import db, Hero
+from models import db, Hero, Power, HeroPower
 
 file_path = os.path.abspath(os.getcwd()) + '/db/app.db'
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
-def home():
-    return ''
+api = Api(app)
 
+class Heroes(Resource):
+    def get(self):
+        heroes = Hero.query.all()
+        heroes_dict = [
+            {
+                "id": hero.id,
+                "name": hero.name,
+                "super_name": hero.super_name
+            } for hero in heroes
+        ]
+        
+        return make_response(jsonify(heroes_dict), 200)
+        
+
+api.add_resource(Heroes, '/heroes')
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5556)
--- app/models.py
+++ app/models.py
@@ -13,7 +13,20 @@ class Hero(db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
 
-    hero_powers = db.relationship('HeroPower', backref='hero')
+    hero_powers = db.relationship('HeroPower', backref='hero', lazy=True)
+
+    def to_dict(self, include_powers=True):
+        hero_dict = {
+            'id': self.id,
+            'name': self.name,
+            'super_name': self.super_name,
+        }
+
+        if include_powers:
+            hero_dict['powers'] = [hero_power.power.to_dict(include_heroes=False) for hero_power in self.hero_powers]
+
+        return hero_dict
+        
 
     def __repr__(self):
         return f'<Hero {self.name}, {self.super_name}>'
@@ -27,13 +40,25 @@ class Power(db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
 
-    hero_powers = db.relationship('HeroPower', backref='power')
+    hero_powers = db.relationship('HeroPower', backref='power', lazy=True)
 
     @validates('name')
     def validate_name(self, key, name):
         if len(name) < 20:
             raise ValueError('Name must be at least 20 characters long')
         return name
+    
+    def to_dict(self, include_heroes=True):
+        power_dict = {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+        }
+
+        if include_heroes:
+            power_dict['heroes'] = [hero_power.hero.to_dict(include_powers=False) for hero_power in self.hero_powers]
+
+        return power_dict
 
     def __repr__(self):
         return f'<Power {self.name}, {self.description}>'
@@ -58,3 +83,4 @@ class HeroPower(db.Model, SerializerMixin):
     def __repr__(self):
         return f'<HeroPower {self.hero_id}, {self.power_id}>'
  
+
