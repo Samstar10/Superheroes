e3ca2c6b2472bbab9c0482dd3068ca9af4626b3b
Samuel Muli <samuelmuli95@gmail.com>
2024-01-14 18:28:24 +0300

    Create Power model

--- app/models.py
+++ app/models.py
@@ -15,5 +15,28 @@ class Hero(db.Model, SerializerMixin):
     def __repr__(self):
         return f'<Hero {self.name}, {self.super_name}>'
     
+class Power(db.Model, SerializerMixin):
+    __tablename__ = 'powers'
 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+    def __repr__(self):
+        return f'<Power {self.name}, {self.description}>'
+    
+class HeroPower(db.Model, SerializerMixin):
+    __tablename__ = 'hero_powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String, nullable=False)
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), nullable=False)
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+    def __repr__(self):
+        return f'<HeroPower {self.hero_id}, {self.power_id}>'
  
